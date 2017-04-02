708bca69db8ee52aaf38d6e36694e59b4368e98d
Kishlin <legrand.pierrelouis@gmail.com>
2017-04-02 17:08:41 +0200

    Project installation.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name movieDatabaseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the movieDatabaseApp
+ */
+angular.module('movieDatabaseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
